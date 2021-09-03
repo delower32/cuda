76ea01713ebe797e92ed1a9825b1a3de0493cbe8
Md Delower Hossain <90022096+delower32@users.noreply.github.com>
2021-09-03 11:56:24 +0600

    please added my file

---
+++ cuda projects/assets/js/main.js
@@ -0,0 +1,11 @@
+
+ $(document).ready(function(){
+     $('.main-nav').meanmenu({
+  
+          meanMenuContainer :'.mobail-menu',
+           meanScreenWidth:720,
+      })
+
+      var mixer = mixitup(".container");
+  
+  });
